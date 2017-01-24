7cba902064dae0c7e135fc95d4284f1c41b2ab4d
unknown <i9100404@CXLDOM00.CATHAY.INS>
2017-01-24 16:06:19 +0800

    add if condition to mamcommand

--- MsMAM.sh
+++ MsMAM.sh
@@ -7,13 +7,18 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+#def function
+function setCustomedVar() {
+	declare -x $1=$2
+}
+
 #define var
 MAMCmd=/Users/prode/Desktop/Contents/MacOS/IntuneMAMPackager
 SHAHashCode=0CFDA9E07D328E164A037E0DBC538DEBE92ADD00
 outputPostfix=$(date +%Y%m%d_%H%M%S)
 
 #Verify workdir
-workdir=D:\\work\\mam
+workdir=D:/temp/mam
 read -p "Input working Directory Default=> ${workdir} " NewWorkDir
 test ! -z ${NewWorkDir} && workdir=${NewWorkDir}
 echo  "Your working ${workdir}"
@@ -21,16 +26,25 @@ echo -e "\n"
 test !  -d ${workdir} && echo "Your workdir is neither a directory nor existing" && exit 0
 
 #Verify source ipa
-read -p "type your source Ipa Name :  " sourceName
-test ! -f ${workdir}/input/${sourceName} && echo "Your source Ipa Doesn't exist => ${workdir}/input/${sourceName}" && exit 0
+inputIpaCount=$(find ${workdir}/input -name "*.ipa" | wc -l)
+#echo inputIpaCount:  ${inputIpaCount}
+
+if [ ${inputIpaCount} -eq 1 ]; then
+	existIPA=$(find ${workdir}/input -name "*.ipa" | while read ipacon ; do echo "$ipacon"  ; done)	
+fi
+#echo existIPA :  $existIPA
+test  -f ${existIPA} && test ! -z ${existIPA}  && read -p "Verify your ipa location or change it : ${existIPA} " inputsourceName || read -p "type your sourceName name : " inputsourceName
+test ! -z ${inputsourceName} && sourceName=${workdir}/input/${inputsourceName} || sourceName=${existIPA}
+
+#read -p "type your source Ipa Name :  " sourceName
+test ! -f ${sourceName} || test -z ${sourceName} && echo "Your source Ipa Doesn't exist => ${workdir}/input/${sourceName}" && exit 0
 
 #Verify profile
-read -p "type your profile name : " profilename
+read -p "type your profile name* : " profilename
 test ! -f ${workdir}/profile/${profilename} && echo "Your profile Doesn't exist =>  ${workdir}/profile/${profilename}"  && exit 0 
 
 
 #Execute MAM command
-${MAMCmd} -i ${workdir}/input/${sourceName} -o ${workdir}/output/${sourceName}_${outputPostfix}  -p ${workdir}/profile/${profilename} -c ${SHAHashCode}  -v true
-
+${MAMCmd} -i ${sourceName} -o ${workdir}/output/$(echo ${sourceName} | sed 's/.*\///g')_${outputPostfix}  -p ${workdir}/profile/${profilename} -c ${SHAHashCode}  -v true
 
 
